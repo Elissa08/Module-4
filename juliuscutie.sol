f54aaf44fcdcb25883cb3070b9228336bd92897e
Elissa08 <128491972+Elissa08@users.noreply.github.com>
2024-05-29 02:36:34 +0800

    Update juliuscutie.sol

--- juliuscutie.sol
+++ juliuscutie.sol
@@ -1,60 +1,79 @@
-    /* This line specify the license under which the contract is published under MIT and 
-    the version of the Solidity compiler that the contract 
-    should be compiled with (version 0.8.0 or higher).*/
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-    // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
+// Compatible with OpenZeppelin Contracts ^5.0.0
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 
+contract Degen is ERC20, Ownable {
+/* An array to store the IDs of items.*/
+    uint256[] internal itemIds;
 
-    //An import statement brings in the ERC20.sol file from the OpenZeppelin library.
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+    /*A mapping from item ID to a struct Value.*/
+    mapping(uint256 => Value) public items;
 
-    /*The "ELISSA" contract is derived from the ERC20 contract. It indicates that the deal for "Degen" will 
-    own every function listed in the ERC20 contract for an ERC20 token.*/ 
-
-contract Degen is ERC20 {
-        
-
-    /*The constructor function is used to initialize the contract when it's deployed. 
-    It has the initial name of the token to "Degen" and its symbol to "DGN".*/
-    constructor() ERC20("Degen", "DGN"){
-
-        /*_mint function is called within the constructor to mint 777 tokens and assign them to the deployer's address 
-        (msg.sender or the owner account).*/
-        _mint (msg.sender, 777);
+    struct Value {
+        string daphcute;
+        uint256 spend;
     }
 
-    /*Mint functions allows the owner to create a new token. */
-    function Mint(uint _token) public {
-        _mint(msg.sender, _token);
+    constructor(address daphcutie)
+        ERC20("Degen", "DGN")
+        Ownable(daphcutie){    
+        _mint (msg.sender, 777);}
+
+/* Allows the owner to create a new item by providing a name and cost. */
+    function createItem(string memory _name, uint256 _cost) external onlyOwner {
+        Value memory newItem = Value({daphcute: _name, spend: _cost});
+        items[itemIds.length] = newItem;
+
+        itemIds.push(itemIds.length);
     }
 
-    
-    /* The caller can send tokens to a recipient address using the transfer function. 
-    To complete the transfer, it makes use of the _transfer method that was inherited from ERC20.*/ 
-     function transfer(address recipient, uint256 _token) public override returns (bool) {
-        _transfer(msg.sender, recipient, _token);
-        return true;
-    }
-    
-    //Redeeming tokens typically refers to exchanging tokens for some form of value, such as rewards, goods, or services. 
-    function redeem(uint _Redeem) public {
-        require(balanceOf(msg.sender) >= _Redeem, "Insufficient balance");
-        _burn(msg.sender, _Redeem);
+/* Allows to create a token */
+    function mint(uint256 balance) public onlyOwner {
+        _mint(msg.sender, balance);
     }
 
-     function redeemtoken(address from, uint256 _token) public {
-    require(balanceOf(from) >= _token, "Insufficient balance");
-    _burn(from, _token);
-    }
-   
-    function checkTokenBalance(address _account) public view returns (uint) {
-        return balanceOf(_account);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
+/* allows to burn token */
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }  
 
-    /*Burn functions allow users to destroy their tokens.*/ 
      function Burn(address from, uint256 _token) public {
-        _burn(from, _token);
+    _burn(from, _token);
+}
+
+/* Allows to transfer the token */
+    function transferTo(address to, uint256 amount) public  {
+        transfer(to, amount);
     }
+
+    function transfer(address receiver, uint256 _amount) public override returns (bool) {
+    _transfer(msg.sender, receiver, _amount);
+    return true;
+}
+
+/* allows the user to check the token balance */
+function checkTokenBalance(address _account) public view returns (uint) {
+    return balanceOf(_account);
+}
+
+/* allows the user to redeem the item using ID value or items*/
+/* Allows any address to redeem an item by ID, 
+transferring the item's cost from the caller to the contract and 
+deleting the item from the mapping. */
+    function redeemItem(uint256 id) external {
+        require(items[id].spend > 0, "Item does not exist!");
+        require(balanceOf(msg.sender) >= items[id].spend, "Insufficient Balance");
+
+        _transfer(msg.sender, address(this), items[id].spend);
+
+        delete items[id];
+    }
+    
 }
